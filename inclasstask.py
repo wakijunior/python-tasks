600cb0885e70c02fe1b1ba1723997093019e3601
Henry <hewstechcomps@gmail.com>
2025-11-06 19:36:29 +0300

    My commit message …

---
+++ inclasstask.py
@@ -0,0 +1,32 @@
+# write a program that reverses a string.
+
+string = "Hello this is python programming"
+
+def reverse_string(str):
+    return str[::-1]
+
+my_reversed_string = reverse_string(string)
+print(my_reversed_string)
+
+# write a python program to print the even numbers from a given list.
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+# def even_number_selector(numbers):
+#     list1 = []
+#     if numbers %2 == 0:
+#         list1.append()
+#         return list1
+
+# even_numbers = even_number_selector(list)   
+# print(even_numbers) 
+
+# write a python program to create and print a list where the values are the square of 
+# numbers between 1 and 30 (both included)
+
+def squares_of_numbers():
+    for i in range(1,31)
+    squares = i ** 2
+    return square
+
+square_of_i = squares_of_numbers()
